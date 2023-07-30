43fe8a6d29bff1813bf177b8edf5322943a78294
ChuxInCode <chuxmonye@gmail.com>
2023-07-30 19:56:02 -0400

    minor upgrade

--- to_do_list_app/src/components/home.jsx
+++ to_do_list_app/src/components/home.jsx
@@ -6,10 +6,13 @@ const styling = {
 }
 
 // might not need this !!!
-const formStyle = {
-	width: "50%",
-	
-	
+const createdTasksStyle = {
+	backgroundColor: "",
+	color: "#7c9bb9"	
+}
+const completedTasksStyle = {
+	backgroundColor: "",
+	color: "#aa0011"
 }
 const Home = ()=>{
 
@@ -19,9 +22,13 @@ const Home = ()=>{
 		<h3>ToDo List</h3>
 
 		<div>
-				<div className="input-group mb-3 w-50 mx-auto">
-					<input type="text" className="form-control" placeholder="Create new Todo" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-					<span className="input-group-text" id="basic-addon2">@example.com</span>
+				<div className="input-group mb-4 w-50 mx-auto">
+					<input type="text" className="form-control" placeholder="Create new Todo" aria-label="input for new todo" aria-describedby="basic-addon2"/>
+					<span className="input-group-text" id="basic-addon2"><button  type="submit" className="btn btn-primary mb-1">Create</button></span>
+				</div>
+				<div className=" row g-3 mb-3 w-50 mx-auto">
+					<div style={createdTasksStyle} className="col">Created Tasks</div>
+					<div style={completedTasksStyle} className="col">Completed Tasks</div>
 				</div>
 		</div>
 
