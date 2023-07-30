4a6f7744b19f829324cc05d6751c0f2667b803f2
ChuxInCode <chuxmonye@gmail.com>
2023-07-30 19:27:55 -0400

    update

--- to_do_list_app/src/components/home.jsx
+++ to_do_list_app/src/components/home.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+const styling = {
+	backgroundColor : "#111111",
+	minHeight: "100vh",
+	color: "#6c8c86"
+}
+
+// might not need this !!!
+const formStyle = {
+	width: "50%",
+	
+	
+}
 const Home = ()=>{
 
 	return(<>
-	<h3>To-Do App</h3></>)
-}
+	<div style={styling} >
+
+		<h3>ToDo List</h3>
+
+		<div>
+				<div className="input-group mb-3 w-50 mx-auto">
+					<input type="text" className="form-control" placeholder="Create new Todo" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+					<span className="input-group-text" id="basic-addon2">@example.com</span>
+				</div>
+		</div>
+
+	</div>
+	</>)
+}	
 export default Home
--- to_do_list_app/src/main.jsx
+++ to_do_list_app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 import Home from './components/home.jsx'
 import bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
